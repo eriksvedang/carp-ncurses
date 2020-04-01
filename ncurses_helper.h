e0cd385372b7c5708b4283e07c062706bab7d10e
Scott Olsen <scg.olsen@gmail.com>
2020-04-01 19:35:18 -0400

    Add attr function family and Attribute type
    This change adds the family of attr functions, described in curs_attr.
    There are two important points to note about these bindings:
    
    - We need to introduce a new type, `Attribute` to mirror the curses
    attr_t type. We can't use attr_t directly because on the Carp side, we
    need to wrap attributes (attr_t values) in the `Ptr` constructor to
    denote pointers. Lowercase values passed to constructors such as Ptr are
    interpreted as type variables, not the concrete type we want here, so we
    need to use an alias.
    
    - Several of these functions take void pointers as a final argument;
    most of them are unused, excepting functions that can change colors
    (according to curs_attr) which instead expect an Int pointer; so,
    instead of using a `(Ptr ())` type, we set the binding signatures to
    `(Ptr Int)` and pass Carp's NULL wherever an Int isn't supported in
    ncurses.

--- ncurses_helper.h
+++ ncurses_helper.h
@@ -1,5 +1,18 @@
 #include <ncurses.h>
 
+// We need to register a separate type for attributes, as several functions need
+// to use pointers to attributes.
+// In Carp, the pointer type Ptr takes an argument; if the value of this
+// argument begins with a lowercase letter, it will be interpreted as a type
+// parameter instead of a concrete type, so, we need to pass it a type that
+// begins with a capital letter.
+typedef attr_t Attribute;
+
+Attribute *NCurses_attr_ptr() {
+  Attribute* attributes;
+  return attributes;
+}
+
 int NCurses_width() {
     int row, col;
     getmaxyx(stdscr, row, col);
